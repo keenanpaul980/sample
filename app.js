988ba8dba0a3f7755093ec9fbd4685768bbb2d82
Keenan <keenanpaul980@gmail.com>
2018-09-28 14:32:45 +0800

    added downloads component

--- app.js
+++ app.js
@@ -7,6 +7,32 @@ const app = express();
 //set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/users', function(req, res) {
+  let users = [
+    {
+      firstName: 'Keenan',
+      age: 20,
+      gender: 'male'
+    },
+    {
+      firstName: 'Paula',
+      age: 21,
+      gender: 'female'
+    },
+    {
+      firstName: 'Juan',
+      age: 22,
+      gender: 'male'
+    }
+
+  ];
+  res.json(users);
+})
+
+app.get('/downloads', function(req, res){
+  res.download(path.join(__dirname, '/downloads/pdf.pdf.pdf'));
+})
+
 
 app.listen(3000, function(req, res){
   console.log('Live server at port 3000');
