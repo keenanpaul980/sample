1788565c81e1b7304231a43be4a1d4a9c86b9a04
Keenan <keenanpaul980@gmail.com>
2018-10-01 16:25:45 +0800

    removed unused function

--- app.js
+++ app.js
@@ -10,28 +10,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 //set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/users', function(req, res) {
-//   let users = [
-//     {
-//       firstName: 'Keenan',
-//       age: 20,
-//       gender: 'male'
-//     },
-//     {
-//       firstName: 'Paula',
-//       age: 21,
-//       gender: 'female'
-//     },
-//     {
-//       firstName: 'Juan',
-//       age: 22,
-//       gender: 'male'
-//     }
-
-//   ];
-//   res.json(users);
-// })
-
 app.get('/downloads', function(req, res){
   res.download(path.join(__dirname, '/downloads/pdf.pdf.pdf'));
 })
@@ -44,6 +22,7 @@ app.post('/subscribe', function(req, res){
   let name = req.body.name;
   let email = req.body.email;
   console.log(`${name} has subscribed to newsletter using ${email}`);
+  res.send()
 })
 
 app.listen(3000, function(req, res){
