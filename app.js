2d626f15397467bebb708e00e26e415bb3b0941c
Keenan <keenanpaul980@gmail.com>
2018-10-01 15:24:48 +0800

    added subscribe.html

--- app.js
+++ app.js
@@ -6,31 +6,31 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use()bodyParser.urlencoded({extended: false});
+app.use(bodyParser.urlencoded({extended: false}));
 //set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/users', function(req, res) {
-  let users = [
-    {
-      firstName: 'Keenan',
-      age: 20,
-      gender: 'male'
-    },
-    {
-      firstName: 'Paula',
-      age: 21,
-      gender: 'female'
-    },
-    {
-      firstName: 'Juan',
-      age: 22,
-      gender: 'male'
-    }
+// app.get('/users', function(req, res) {
+//   let users = [
+//     {
+//       firstName: 'Keenan',
+//       age: 20,
+//       gender: 'male'
+//     },
+//     {
+//       firstName: 'Paula',
+//       age: 21,
+//       gender: 'female'
+//     },
+//     {
+//       firstName: 'Juan',
+//       age: 22,
+//       gender: 'male'
+//     }
 
-  ];
-  res.json(users);
-})
+//   ];
+//   res.json(users);
+// })
 
 app.get('/downloads', function(req, res){
   res.download(path.join(__dirname, '/downloads/pdf.pdf.pdf'));
@@ -41,7 +41,9 @@ app.get('/about', function(req, res){
 })
 
 app.post('/subscribe', function(req, res){
-
+  let name = req.body.name;
+  let email = req.body.email;
+  console.log(`${name} has subscribed to newsletter using ${email}`);
 })
 
 app.listen(3000, function(req, res){
